6fea063be8534adc845e4a9782393dd357bb2ed1
Rick Bruyere <rick.bruyere@gmail.com>
2016-11-30 22:10:02 -0500

    CustomerViewModelUpdatedForFavorites

--- RezzQueue/Models/CustomerViewModel.cs
+++ RezzQueue/Models/CustomerViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,39 +10,8 @@ namespace RezzQueue.Models
     {
         public Customer Customer { get; set; }
         public IEnumerable<Animal> AllAnimals { get; set; }
-        public IEnumerable<PetStatus> AllPetstatus { get; set; }
-
-        private List<int> _selectedAnimals;
-        public List<int> SelectedAnimals {
-            get
-            {
-                if (_selectedAnimals == null)
-                {
-                   
-                    _selectedAnimals = (from a in Customer.Animals
-                                        select a.AnimalId).ToList();
-                }
-                return _selectedAnimals;
-            }
-            set { _selectedAnimals = value; }
-        }
-    
-
-        private List<int> _selectedPetStatus;
-        public List<int> SelectedPetStatus
-{
-    get
-    {
-        if (_selectedPetStatus == null)
-        {
-          
-            _selectedPetStatus = (from a in Customer.PetStatuses
-                                   select a.PetStatusId).ToList();
-        }
-        return _selectedPetStatus;
-    }
-    set { _selectedPetStatus = value; }
-}
+        public IEnumerable<PetStatus> PetStatus { get; set; }
+        
 
     }
 }
